23f8083833ea6661a475ba6bd46bc65da2ed2012
Stefanie <stefcodes@gmail.com>
2020-03-17 00:11:32 -0700

    add search, song, and song view path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Search from './containers/Search';
+import SongItem from './components/SongItem';
+import SongView from './components/SongView'; 
 
 
 class App extends React.Component {
@@ -9,6 +12,9 @@ class App extends React.Component {
     <div>
       <Router>
         <Route exact path={"/"} component={HomePage} />
+        <Route exact path={"/search"} component={Search} />
+        <Route exact path={"/song"} component={SongItem} />
+        <Route exact path="/song/:trackId" component={SongView} />
       </Router>
     </div>
     )

7525b57cc99cd1f14b211b5366193be4e94acb68
Stefanie <stefcodes@gmail.com>
2020-03-17 14:54:11 -0700

    add fetch to getSongs action

--- src/actions/song.js
+++ src/actions/song.js
@@ -1,8 +1,13 @@
 export const getSongs = () => {
     return (dispatch) => {
         dispatch({type:"LOADING_SONGS"})
-    }
-}
+        fetch("http://localhost:3001/songs")
+        .then(response => response.json())
+        .then(data => {
+          return dispatch({ type: "SONGS_LOADED", payload: data });
+        });
+    };
+  };
 
 export const addSong = song => {
     return dispatch => {

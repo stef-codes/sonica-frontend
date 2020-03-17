c372042e90efb153d86bd50073faba6f75c9f21d
Stefanie <stefcodes@gmail.com>
2020-03-17 00:12:09 -0700

    create song actions

---
+++ src/actions/song.js
@@ -0,0 +1,24 @@
+export const getSongs = () => {
+    return (dispatch) => {
+        dispatch({type:"LOADING_SONGS"})
+    }
+}
+
+export const addSong = song => {
+    return dispatch => {
+        dispatch({type: "LOADING_SONGS"})
+        return fetch(`http://localhost:3001/songs`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+            body: JSON.stringify(song)
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            dispatch({type: "ADD_SONG", payload: data})
+        })
+    }
+}
+

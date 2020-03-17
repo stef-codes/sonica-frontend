7c95c89ac199c6429196ad23d7d9f10bef874300
Stefanie <stefcodes@gmail.com>
2020-03-17 00:13:05 -0700

    create song item component

---
+++ src/components/SongItem.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+
+class SongItem extends Component {
+
+  render() {
+    const song = this.props.song;
+
+    return (
+      <div data-id={song.trackId}>
+          <img src={song.artworkUrl100} alt={`${song.trackName}`} />
+          <h3>
+            <Link
+              to={{
+                pathname: `/song/${song.trackId}`,
+                state: { song }
+              }}
+            >
+              {song.trackName}
+            </Link>
+          </h3>
+          {/* <p>{song.synopsis}</p>
+          <p>{song.synopsis}</p> */}
+      </div>
+    );
+  }
+}
+
+
+export default SongItem

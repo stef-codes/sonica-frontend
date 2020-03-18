5a951723b1d65f17e988cd599cba0e1977f73730
Stefanie <stefcodes@gmail.com>
2020-03-17 20:08:56 -0700

    add button to HomePage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 // import '../homepage.styles.css'
 
 const HomePage = () => {
     return (
     <div>
         <h1>Welcome to Sonica</h1>
-        {/* <Link to="/playlists"><button>See Playlists</button></Link> */}
+        <Link to="/search"><button>Search</button></Link>
     </div>
     )
 }

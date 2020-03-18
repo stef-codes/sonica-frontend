223242d35938cf629147905280e06113a481d94c
Stefanie <stefcodes@gmail.com>
2020-03-18 14:26:29 -0700

    add React Player

--- src/containers/SongView.jsx
+++ src/containers/SongView.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/SongView.styles.css'
 import { connect } from 'react-redux';
 import { addSong } from '../actions/song'; 
+import ReactPlayer from 'react-player'
 
 class SongView extends React.Component {
   state = {
@@ -11,26 +12,40 @@ class SongView extends React.Component {
         primaryGenreName: this.props.location.state.song.primaryGenreName, 
         artworkUrl100: this.props.location.state.song.artworkUrl100,
         trackViewUrl: this.props.location.state.song.trackViewUrl, 
-        previewUrl: this.props.location.state.song.previewUrl
+        previewUrl: this.props.location.state.song.previewUrl,
+        playing: false
   }
 
   handleClick = song => {
-      debugger
     this.props.addSong(song).then(() =>
     this.props.history.push("/saved"))
   };
+
+  handlePlay = () => {
+    console.log('onPlay')
+    this.setState({ playing: true })
+  }
+
+  handlePause = () => {
+    console.log('onPause')
+    this.setState({ playing: false })
+  }
 
 
   render() {
      const song = this.state;
     return (
       <div className="track">
-          <img src={song.artworkUrl100} alt={song.trackName} />
+        <ReactPlayer url={song.previewUrl} controls='true'>
+        </ReactPlayer>
+
+
+        <img src={song.artworkUrl100} alt={song.trackName} />
           <h3>{song.trackName}</h3>
           <p>Genre: {song.primaryGenreName}</p>
-            <button id={this.props.match.params.trackId} onClick={() => this.handleClick(song)}>
-              Add to List
-            </button>
+          <button id={this.props.match.params.trackId} onClick={() => this.handleClick(song)}>
+            Add to List
+          </button>
       </div>
     );
   }

fde31b545e8b80cc2edc7ae4e6f20c2e63eefb6b
Stefanie <stefcodes@gmail.com>
2020-03-20 20:41:05 -0700

    add vale back to search

--- src/containers/Search.jsx
+++ src/containers/Search.jsx
@@ -61,11 +61,11 @@ class Search extends React.Component {
         
         return (
             <div>
-            <form onSubmit={this.handleSubmit} >
-                <h3>Search for a Song</h3>
-                <input type="text" name="searchTerm" onChange={this.handleChange} placeholder="Nipsey Hussle" />
-                <button type="submit">Search</button> 
-            </form>
+                <form onSubmit={this.handleSubmit} >
+                    <h3>Search for a Song</h3>
+                    <input type="text" name="searchTerm" value={this.state.searchTerm} onChange={this.handleChange} placeholder="Nipsey Hussle" />
+                    <button type="submit">Search</button> 
+                </form>
                <br></br>
         
                <div>

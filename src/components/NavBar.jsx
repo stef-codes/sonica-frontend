18d1da68b230054811b186f357080473dd71125c
Stefanie <stefcodes@gmail.com>
2020-03-28 00:05:12 -0700

    change navigation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import Nav from 'react-bootstrap/Nav'
 
-const links = {
-    width: '100px',
-    padding: '12px',
-    margin: '0 6px 6px',
-    background: '#abb8c3',
-    color: 'white',
-    borderRadius: '5px'
-}
+// const links = {
+//     width: '100px',
+//     padding: '12px',
+//     margin: '0 6px 6px',
+//     background: '#abb8c3',
+//     color: 'white',
+//     borderRadius: '5px'
+// }
 
 
 const NavBar = () => {
     return (
-      <div className="navbar">
-        <NavLink to="/" style={links}>Home</NavLink>
-        <NavLink to="/search" style={links}>Search</NavLink>
-        <NavLink to="/songs" style={links}>Songs</NavLink>
-        <NavLink to="/journal" style={links}>Journal</NavLink>
-      </div>
+      <Nav fill variant="tabs" >
+        <Nav.Item>
+          <Nav.Link href="/">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/search">Search</Nav.Link>  
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/songs" >Songs</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/journal" >Journal</Nav.Link>
+        </Nav.Item>
+      </Nav>
     );
 };
 
